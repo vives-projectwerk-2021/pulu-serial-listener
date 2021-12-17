e7aeb4ef8e39ef69e317e5ccba0745a77c3bcaca
Dimi Catrysse <dimicatrysse@outlook.com>
2021-12-17 15:24:54 +0100

    Refactored to listen blocking/non-blocking and listen for reset command

--- src/SerialListener.cpp
+++ src/SerialListener.cpp
@@ -5,6 +5,13 @@
 #include "mbedtls/base64.h"
 
 namespace Pulu {
+    SerialListener::SerialListener(BufferedSerial* bufferedSerial) {
+        this->bufferedSerial = bufferedSerial;
+    }
+    SerialListener::SerialListener(USBSerial* usb) {
+        this->usb = usb;
+    }
+
     bool SerialListener::cmd_IS_request_id() {
         char cmd[4];
         read(cmd, sizeof(cmd));
@@ -15,13 +22,23 @@ namespace Pulu {
         return memcmp(decodedCmd, search, sizeof(search)) == 0;
     }
 
+    bool SerialListener::cmd_IS_reset() {
+        char cmd[4];
+        read(cmd, sizeof(cmd), true);
+        char decodedCmd[3];
+        size_t olen;
+        mbedtls_base64_decode((unsigned char*)decodedCmd, sizeof(decodedCmd), &olen, (unsigned char*)cmd, sizeof(cmd));
+        char search[3] = {'r','e','s'};
+        return memcmp(decodedCmd, search, sizeof(search)) == 0;
+    }
+
     void SerialListener::print_id() {
         auto uid = get_uid_bytes();
         size_t encoded_size;
         mbedtls_base64_encode(nullptr, 0, &encoded_size, uid.data(), uid.size());
         char* encodedUid = (char*) malloc(encoded_size);
         mbedtls_base64_encode((unsigned char*)encodedUid, encoded_size, &encoded_size, uid.data(), uid.size());
-        printf("%s\n", encodedUid);
+        printf("%s\r\n", encodedUid);
         free(encodedUid);
     }
 
@@ -31,13 +48,13 @@ namespace Pulu {
 
         EEPROM_Config config;
         char conf[BASE64_CONF_LENGTH];
-        read(conf, sizeof(conf));
+        read(conf, sizeof(conf), true);
 
         size_t decoded_size;
         mbedtls_base64_decode(nullptr, 0, &decoded_size, (unsigned char*)conf, sizeof(conf));
 
         if(decoded_size!=CONF_LENGTH) {
-            printf("1\n");
+            printf("1\r\n");
             error = true;
             return config;
         }
@@ -54,17 +71,54 @@ namespace Pulu {
         wait_time = __REV16(wait_time); // big endian to little endian
         memcpy(&config.wait_time, &wait_time, 2);
 
-        printf("0\n");
+        printf("0\r\n");
         error = false;
         return config;
     }
 
 
-    void SerialListener::read(char* data, uint8_t length) {
+    void SerialListener::read(char* data, uint8_t length, bool blocking) {
+        const uint8_t MAX_WAIT_TIME_SECONDS = 20;
+        const uint8_t TRY_INTERVAL_MS = 200;
+        const uint8_t MAX_TRY_COUNT = (1000/TRY_INTERVAL_MS)*MAX_WAIT_TIME_SECONDS;
         for(uint8_t i = 0; i<length; i++) {
-            ssize_t ret;
-            do { ret = mbed_file_handle(STDOUT_FILENO)->read(&data[i], 1); }
-            while(ret == 0); // make sure data is available when non-blocking
+            if(blocking) {
+                ssize_t ret;
+                do { ret = mbed_file_handle(STDIN_FILENO)->read(&data[i], 1); }
+                while(ret == 0);
+            }
+            else if(usb) {
+                uint8_t try_count = 0;
+                do {
+                    if(usb->readable())
+                    {
+                        usb->read(&data[i], 1);
+                        break;
+                    }
+                    else
+                    {
+                        ThisThread::sleep_for(TRY_INTERVAL_MS*1ms);
+                    }
+                }
+                while(try_count++<MAX_TRY_COUNT);
+                if(try_count>MAX_TRY_COUNT) return;
+            }
+            else if(bufferedSerial) {
+                uint8_t try_count = 0;
+                do {
+                    if(bufferedSerial->readable())
+                    {
+                        bufferedSerial->read(&data[i], 1);
+                        break;
+                    }
+                    else
+                    {
+                        ThisThread::sleep_for(TRY_INTERVAL_MS*1ms);
+                    }
+                }
+                while(try_count++<MAX_TRY_COUNT);
+                if(try_count>MAX_TRY_COUNT) return;
+            }
         }
     }
 };
--- src/SerialListener.h
+++ src/SerialListener.h
@@ -1,14 +1,24 @@
 #pragma once
 
+#include "USBSerial.h"
+#include "BufferedSerial.h"
 #include "eepromConfig.h"
 
 namespace Pulu {
     class SerialListener {
         public:
+            SerialListener(BufferedSerial* bufferedSerial);
+            SerialListener(USBSerial* usb);
+        public:
             bool cmd_IS_request_id();
+            bool cmd_IS_reset();
             void print_id();
             EEPROM_Config read_conf(bool &error);
         private:
-            void read(char* data, uint8_t length);
+            void read(char* data, uint8_t length, bool blocking = false);
+
+        private:
+            BufferedSerial* bufferedSerial = nullptr;
+            USBSerial* usb = nullptr;
     };
 };
